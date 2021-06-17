ecc853e151ce5713bcce0aa758b09ba35794c575
Sam Wachspress <samuel.wachspress@northwestern.edu>
2021-06-17 15:15:16 -0500

    query pipeline status and update datastore entry on pubsub message

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ import (
 	"golang.org/x/net/context"
 	"golang.org/x/oauth2/google"
 	"google.golang.org/api/googleapi"
+	"google.golang.org/api/iterator"
 	lifesciences "google.golang.org/api/lifesciences/v2beta"
 	"google.golang.org/api/option"
 )
@@ -25,15 +26,21 @@ const XCloudtasksQueuename = "nscalc"
 const SERVICE_ACCOUNT = "nscalc-201573431837@andersen-lab.iam.gserviceaccount.com"
 const SCOPE = "https://www.googleapis.com/auth/cloud-platform"
 
-const IMAGE_URI = "northwesternmti/nemarun:0.51"
+const IMAGE_URI = "northwesternmti/nemarun:0.60"
 const PUB_SUB_TOPIC = "projects/andersen-lab/topics/nemarun"
 
 const MACHINE_TYPE = "n1-standard-4"
+const PREEMPTIBLE = true
 const REGION = "us-central1"
 const TIMEOUT = "86400s"
 const BOOT_IMAGE = "projects/cos-cloud/global/images/family/cos-stable"
-
 const PARENT = "projects/201573431837/locations/us-central1"
+const NS_DATA_PATH = "gs://elegansvariation.org/reports/nemascan"
+const NS_WORK_PATH = "gs://nf-pipelines/workdir"
+const LOCAL_WORK_PATH = "/workdir"
+const VOLUME_NAME = "nf-pipeline-work"
+
+const DS_OPERATION_KIND = "gls_operation"
 
 type Payload struct {
 	Hash    string
@@ -46,6 +53,36 @@ type dsInfo struct {
 	Id        string
 	Msg       string
 	Data_hash string
+}
+
+type operationOptions struct {
+	Data_path       string `datastore:"data_path"`
+	Work_path       string `datastore:"work_path"`
+	Container_Name  string `datastore:"container_name"`
+	Volume_Name     string `datastore:"volume_name"`
+	Image_URI       string `datastore:"image_uri"`
+	Machine_Type    string `datastore:"machine_type"`
+	Preemptible     bool   `datastore:"preemptible"`
+	Region          string `datastore:"region"`
+	Timeout         string `datastore:"timeout"`
+	Boot_Image      string `datastore:"boot_image"`
+	Project_ID      string `datastore:"project_id"`
+	Service_Account string `datastore:"service_account"`
+	Scope           string `datastore:"scope"`
+	Parent          string `datastore:"parent"`
+	PubSub_Topic    string `datastore:"pubsub_topic"`
+}
+
+type dsOperationEntry struct {
+	Options     *operationOptions `datastore:"options,noindex"`
+	Done        bool              `datastore:"done"`
+	Error       bool              `datastore:"error"`
+	Data_hash   string            `datastore:"data_hash"`
+	Operation   string            `datastore:"operation"`
+	Report_path string            `datastore:"report_path,noindex"`
+	Modified_on time.Time         `datastore:"modified_on"`
+	Created_on  time.Time         `datastore:"created_on"`
+	K           *datastore.Key    `datastore:"__key__"`
 }
 
 type dsEntry struct {
@@ -68,7 +105,6 @@ type Attr struct {
 }
 
 // PubSubMessage is the payload of a Pub/Sub event.
-// See the documentation for more details:
 // https://cloud.google.com/pubsub/docs/reference/rest/v1/PubsubMessage
 type PubSubMessage struct {
 	Message struct {
@@ -84,38 +120,109 @@ func check(e error, i *dsInfo) {
 	if e != nil {
 		msg := e.Error() + "\n" + i.Msg
 		log.Printf("ERROR: %s", e.Error())
-		setDatastoreStatus(i, "ERROR", msg, "")
+		setDatastoreStatus(i, "ERROR", msg)
 		panic(e)
 	}
 }
 
-// helper function to update status in datastore
-func setDatastoreStatus(i *dsInfo, status string, msg string, op string) {
+func createOperationDSEntry(dsOp *dsOperationEntry) {
 	ctx := context.Background()
 	dsClient, err := datastore.NewClient(ctx, PROJECT_ID)
 	if err != nil {
-		log.Fatal(err)
+		log.Printf("dsClient.NewClient: %v", err)
+	}
+	defer dsClient.Close()
+
+	taskKey := datastore.NameKey(DS_OPERATION_KIND, dsOp.Data_hash, nil)
+	_, txErr := dsClient.RunInTransaction(ctx, func(tx *datastore.Transaction) error {
+		// We first check that there is no entity stored with the given key.
+		var empty dsOperationEntry
+		if err := tx.Get(taskKey, &empty); err != datastore.ErrNoSuchEntity {
+			return err
+		}
+		// If there was no matching entity, store it now.
+		_, err := tx.Put(taskKey, dsOp)
+		return err
+	})
+	if txErr != nil {
+		log.Printf("ERROR CREATING OPERATION DS ENTRY: %s", txErr.Error())
+	}
+}
+
+func updateOperationDSEntry(opID string, opDone bool, opErr bool) {
+	ctx := context.Background()
+	dsClient, err := datastore.NewClient(ctx, PROJECT_ID)
+	if err != nil {
+		log.Printf("dsClient.NewClient: %v", err)
+	}
+	defer dsClient.Close()
+
+	// query for matching data_hash
+	var data_hash = ""
+	query := datastore.NewQuery(DS_OPERATION_KIND).Filter("Operation =", opID)
+	it := dsClient.Run(ctx, query)
+	for {
+		var opEntry dsOperationEntry
+		_, err := it.Next(&opEntry)
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			log.Printf("Error fetching next task: %v", err)
+		}
+		log.Printf("OPERATION: %s\n HASH: %s\n", opEntry.Operation, opEntry.Data_hash)
+		data_hash = opEntry.Data_hash
+	}
+
+	// Update DS entry
+	if data_hash != "" {
+		opKey := datastore.NameKey(DS_OPERATION_KIND, data_hash, nil)
+		tx, err := dsClient.NewTransaction(ctx)
+		if err != nil {
+			log.Printf("client.NewTransaction: %v", err)
+		}
+		var dsOp dsOperationEntry
+		if err := tx.Get(opKey, &dsOp); err != nil {
+			log.Printf("tx.Get: %v", err)
+		}
+
+		dsOp.Modified_on = time.Now()
+		dsOp.Done = opDone
+		dsOp.Error = opErr
+		if _, err := tx.Put(opKey, &dsOp); err != nil {
+			log.Printf("tx.Put: %v", err)
+		}
+		if _, err := tx.Commit(); err != nil {
+			log.Printf("tx.Commit: %v", err)
+		}
+	}
+
+}
+
+// helper function to update status in datastore
+func setDatastoreStatus(i *dsInfo, status string, msg string) {
+	ctx := context.Background()
+	dsClient, err := datastore.NewClient(ctx, PROJECT_ID)
+	if err != nil {
+		log.Printf("client.NewClient: %v", err)
 	}
 	defer dsClient.Close()
 
 	k := datastore.NameKey(i.Kind, i.Id, nil)
 	e := new(dsEntry)
 	if err := dsClient.Get(ctx, k, e); err != nil {
-		log.Fatal(err)
+		log.Printf("client.Get: %v", err)
 	}
 
 	e.Status = status
 	e.Status_msg = msg
-	if op != "" {
-		e.Operation = op
-	}
 
 	if _, err := dsClient.Put(ctx, k, e); err != nil {
-		log.Fatal(err)
+		log.Printf("client.Put: %v", err)
 	}
 
-	fmt.Printf("Updated status: %q\n", e.Status)
-	fmt.Printf("Updated status msg: %q\n", e.Status_msg)
+	log.Printf("Updated status: %q\n", e.Status)
+	log.Printf("Updated status msg: %q\n", e.Status_msg)
 }
 
 func verifyHeader(r *http.Request) bool {
@@ -162,6 +269,28 @@ func extractPayload(r *http.Request) (dsInfo, error) {
 	return info, nil
 }
 
+func generatePipelineOpts(Data_hash string) *operationOptions {
+	// Configure the options for the pipeline
+	NS_CONTAINER_NAME := fmt.Sprintf("nemarun-%s", Data_hash)
+	return &operationOptions{
+		Data_path:       NS_DATA_PATH,
+		Work_path:       NS_WORK_PATH,
+		Container_Name:  NS_CONTAINER_NAME,
+		Volume_Name:     VOLUME_NAME,
+		Image_URI:       IMAGE_URI,
+		Machine_Type:    MACHINE_TYPE,
+		Preemptible:     PREEMPTIBLE,
+		Region:          REGION,
+		Timeout:         TIMEOUT,
+		Boot_Image:      BOOT_IMAGE,
+		Project_ID:      PROJECT_ID,
+		Service_Account: SERVICE_ACCOUNT,
+		Scope:           SCOPE,
+		Parent:          PARENT,
+		PubSub_Topic:    PUB_SUB_TOPIC,
+	}
+}
+
 func nsHandler(w http.ResponseWriter, r *http.Request) {
 	//	Handler for google cloud task which starts the NemaScan nextflow pipeline
 	if !verifyHeader(r) {
@@ -174,14 +303,31 @@ func nsHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// Log & output details of the task.
-	setDatastoreStatus(&info, "STARTING", "", "")
+	setDatastoreStatus(&info, "STARTING", "")
 	log.Printf("INITIALIZING task: KIND: %s, ID: %s, HASH: %s", info.Kind, info.Id, info.Data_hash)
 
+	// Generate struct of options for the Pipeline
+	pOpts := generatePipelineOpts(info.Data_hash)
+
 	// Start the nextflow pipeline
-	operationID := executeRunPipelineRequest(&info)
+	operationID := executeRunPipelineRequest(&info, pOpts)
+
+	// Store the operation info in DataStore
+	pDSOperations := &dsOperationEntry{
+		Options:     pOpts,
+		Done:        false,
+		Error:       false,
+		Data_hash:   info.Data_hash,
+		Operation:   operationID,
+		Report_path: "",
+		Modified_on: time.Time{},
+		Created_on:  time.Now(),
+		K:           &datastore.Key{},
+	}
+	createOperationDSEntry(pDSOperations)
 
 	// Log & output details of the task.
-	setDatastoreStatus(&info, "RUNNING", "", operationID)
+	setDatastoreStatus(&info, "RUNNING", operationID)
 	log.Printf("RUNNING task: KIND: %s, ID: %s, HASH: %s", info.Kind, info.Id, info.Data_hash)
 
 	if errJSONEncoder := json.NewEncoder(w).Encode("Submitted NemaScan"); errJSONEncoder != nil {
@@ -212,12 +358,9 @@ func statusHandler(w http.ResponseWriter, r *http.Request) {
 	getPipelineStatus(m.Message.Attributes.Operation)
 }
 
-func generateRunPipelineRequest(i *dsInfo) *lifesciences.RunPipelineRequest {
+func generateRunPipelineRequest(i *dsInfo, pOpts *operationOptions) *lifesciences.RunPipelineRequest {
 	NS_ID := i.Data_hash
-	NS_DATA_PATH := "gs://elegansvariation.org/reports/nemascan"
-	NS_WORK_PATH := "gs://nf-pipelines/workdir"
-	NS_CONTAINER_NAME := fmt.Sprintf("nemarun-%s", i.Data_hash)
-	LOCAL_WORK_PATH := "/workdir"
+	NS_CONTAINER_NAME := fmt.Sprintf("nemarun-%s", NS_ID)
 
 	pServiceAccount := &lifesciences.ServiceAccount{
 		Email:           SERVICE_ACCOUNT,
@@ -227,7 +370,7 @@ func generateRunPipelineRequest(i *dsInfo) *lifesciences.RunPipelineRequest {
 	}
 
 	pMount := &lifesciences.Mount{
-		Disk:            "nf-pipeline-work",
+		Disk:            VOLUME_NAME,
 		Path:            LOCAL_WORK_PATH,
 		ReadOnly:        false,
 		ForceSendFields: []string{},
@@ -242,7 +385,7 @@ func generateRunPipelineRequest(i *dsInfo) *lifesciences.RunPipelineRequest {
 
 	pVolume := &lifesciences.Volume{
 		PersistentDisk:  pPersistentDisk,
-		Volume:          "nf-pipeline-work",
+		Volume:          VOLUME_NAME,
 		ForceSendFields: []string{},
 		NullFields:      []string{},
 	}
@@ -254,7 +397,7 @@ func generateRunPipelineRequest(i *dsInfo) *lifesciences.RunPipelineRequest {
 		EnableStackdriverMonitoring: true,
 		Labels:                      map[string]string{},
 		MachineType:                 MACHINE_TYPE,
-		Preemptible:                 true,
+		Preemptible:                 PREEMPTIBLE,
 		ServiceAccount:              pServiceAccount,
 		Volumes:                     []*lifesciences.Volume{pVolume},
 		ForceSendFields:             []string{},
@@ -300,6 +443,24 @@ func generateRunPipelineRequest(i *dsInfo) *lifesciences.RunPipelineRequest {
 		NullFields:      []string{},
 	}
 
+	*pOpts = operationOptions{
+		Data_path:       NS_DATA_PATH,
+		Work_path:       NS_WORK_PATH,
+		Container_Name:  NS_CONTAINER_NAME,
+		Volume_Name:     VOLUME_NAME,
+		Image_URI:       IMAGE_URI,
+		Machine_Type:    MACHINE_TYPE,
+		Preemptible:     PREEMPTIBLE,
+		Region:          REGION,
+		Timeout:         TIMEOUT,
+		Boot_Image:      BOOT_IMAGE,
+		Project_ID:      PROJECT_ID,
+		Service_Account: SERVICE_ACCOUNT,
+		Scope:           SCOPE,
+		Parent:          PARENT,
+		PubSub_Topic:    PUB_SUB_TOPIC,
+	}
+
 	return &lifesciences.RunPipelineRequest{
 		Labels:          map[string]string{},
 		Pipeline:        pPipeline,
@@ -314,39 +475,44 @@ func getPipelineStatus(op string) {
 
 	gClient, gClientErr := google.DefaultClient(ctx, lifesciences.CloudPlatformScope)
 	if gClientErr != nil {
-		log.Fatal(gClientErr)
+		log.Printf("glsClient.DefaultClient: %v", gClientErr)
 	}
 
 	glsService, glsServiceErr := lifesciences.NewService(ctx, option.WithHTTPClient(gClient))
 	if glsServiceErr != nil {
-		log.Fatal(glsServiceErr)
+		log.Printf("glsClient.NewService: %v", glsServiceErr)
 	}
 
 	resp, err := glsService.Projects.Locations.Operations.Get(op).Context(ctx).Do()
 	if err != nil {
-		log.Fatal(err)
+		log.Printf("glsClient.Projects.Locations.Operations.Get().Context().Do(): %v", err)
 	}
-	var opDone = "FALSE"
+	var opDone = false
 	if resp.Done {
-		opDone = "TRUE"
+		opDone = true
 	}
-	opError := resp.Error
+	var opErr = false
+	if resp.Error != nil {
+		opErr = true
+	}
 	opName := resp.Name
 
 	// TODO: Change code below to process the `resp` object:
-	fmt.Printf("STATUS - NAME:%s DONE:%s ERROR:%#v\n", opName, opDone, opError)
+	fmt.Printf("STATUS - NAME:%s DONE:%v ERROR:%#v\n", opName, opDone, opErr)
+	updateOperationDSEntry(op, opDone, opErr)
+
 }
 
-func executeRunPipelineRequest(i *dsInfo) string {
+func executeRunPipelineRequest(pInfo *dsInfo, pOpts *operationOptions) string {
 	ctx := context.Background()
 
 	gClient, gClientErr := google.DefaultClient(ctx, lifesciences.CloudPlatformScope)
-	check(gClientErr, i)
+	check(gClientErr, pInfo)
 
 	glsService, glsServiceErr := lifesciences.NewService(ctx, option.WithHTTPClient(gClient))
-	check(glsServiceErr, i)
+	check(glsServiceErr, pInfo)
 
-	runPipelineRequest := generateRunPipelineRequest(i)
+	runPipelineRequest := generateRunPipelineRequest(pInfo, pOpts)
 	pOperation := &lifesciences.Operation{
 		Done:            false,
 		Error:           &lifesciences.Status{},
@@ -358,7 +524,7 @@ func executeRunPipelineRequest(i *dsInfo) string {
 	}
 
 	pOperation, pipelineRunErr := glsService.Projects.Locations.Pipelines.Run(PARENT, runPipelineRequest).Context(ctx).Do()
-	check(pipelineRunErr, i)
+	check(pipelineRunErr, pInfo)
 
 	// TODO: check server response code
 	operationID := pOperation.Name
